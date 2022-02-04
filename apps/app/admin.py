d18c4f416fee1f7ba6304cb7280c5b424fef56a8
land8650@mail.ru <land8650@mail.ru>
2022-02-04 20:10:38 +0500

    v0.1

--- apps/app/admin.py
+++ apps/app/admin.py
@@ -1,28 +1,44 @@
 from typing import Optional
-from django.core.handlers.wsgi import WSGIRequest
+
 from django.contrib import admin
+from django.core.handlers.wsgi import WSGIRequest
 
 from . models import Account
+from . models import Group
+from . models import Student
 
 class AccountAdmin(admin.ModelAdmin):
-    
+
     readonly_fields = ()
 
     def get_readonly_fields(
-        self,
+        self, 
         request: WSGIRequest,
-        obj:Optional[Account] = None
-        ) -> tuple:
-    
-        #if obj is not None:
+        obj: Optional[Account] = None
+    ) -> tuple:
+
         if obj:
             return self.readonly_fields + ('description',)
         return self.readonly_fields
-       
+
+class GroupAdmin(admin.ModelAdmin):
+    
+    readonly_fields = (
+        'name',
+    )
+
+class StudentAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'age',
+    )
+
 
 admin.site.register(
     Account, AccountAdmin
 )
-
-
-# Register your models here.
+admin.site.register(
+    Group, GroupAdmin
+)
+admin.site.register(
+    Student, StudentAdmin
+)
--- apps/app/models.py
+++ apps/app/models.py
@@ -1,28 +1,28 @@
 from django.db import models
-from django.conf import settings
+
+from django.conf import settings 
 from django.contrib.auth.models import User
 
-#class User(models.Model):
- #   pass
-
-
-
+# class User(models.Model):
+#     pass
 class Account(models.Model):
-    ACCOUNT_FULL_NAME_MAX_LENGHT = 20
+
+    ACCOUNT_FULL_NAME_MAX_LENGTH = 20
+
     user = models.OneToOneField(
         User,
-        on_delete = models.CASCADE
+        on_delete=models.CASCADE
     )
     full_name = models.CharField(
-        max_length = ACCOUNT_FULL_NAME_MAX_LENGHT
+        max_length=ACCOUNT_FULL_NAME_MAX_LENGTH
     )
-    
     description = models.TextField()
 
     def __str__(self) -> str:
-        return f'Account: {self.user.id} / {self.full_name}'
+        return f'Аккаунт:{self.user.id}/{self.full_name}'
 
     class Meta:
+
         ordering = (
             'full_name',
         )
@@ -30,5 +30,53 @@ class Account(models.Model):
         verbose_name_plural = 'Аккаунты'
 
 
+class Group(models.Model):
 
-# Create your models here.
+    CROUP_MAX_LENGTH = 10
+
+    name = models.CharField(
+        max_length=CROUP_MAX_LENGTH
+    )
+
+    def __str__(self) -> str:
+        return f'Группа:{self.name}'
+
+    class Meta:
+
+        ordering = (
+            'name',
+        )
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+
+class Student(models.Model):
+
+    account = models.OneToOneField(
+        Account,
+        on_delete=models.CASCADE
+    )
+    age = models.IntegerField(
+        'Возраст студента'
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.PROTECT
+    )
+    gpa = models.FloatField(
+        'Средний балл'
+    )
+
+    def __str__(self) -> str:
+        return f'Аккунт:{self.account}',
+        f'Группа:{self.group.name}',
+        f'Возраст:{self.age}',
+        f'GPA:{self.gpa}'
+
+    class Meta:
+
+        ordering = (
+            'account',
+        )
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
